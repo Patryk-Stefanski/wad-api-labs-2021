acaaf3f9ac95fc3479abae9e186bdb55eaba4615
Patryk-Stefanski <patrykstefanski00@gmail.com>
2021-11-18 15:42:29 +0000

    Week 9 User Favourites

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -19,7 +19,7 @@ router.post('/', async (req, res) => {
 });
 
   // register(Create)/Authenticate User
-  router.post('/', async (req, res) => {
+router.post('/', async (req, res) => {
     if (req.query.action === 'register') {  //if action is 'register' then save to DB
         await User(req.body).save();
         res.status(201).json({
@@ -47,6 +47,29 @@ router.post('/', async (req, res) => {
         res.status(200).json({ code:200, msg: 'User Updated Sucessfully' });
     } else {
         res.status(404).json({ code: 404, msg: 'Unable to Update User' });
+    }
+});
+
+router.post('/:id/favourites', async (req, res) => {
+    const newFavourite = req.body;
+    if (newFavourite && newFavourite.id) {
+        const user = await User.findById(req.params.id);
+        if (user) {
+            user.favourites.push(newFavourite);
+            user.save();
+            res.status(201).json({ code: 201, msg: "Added Favourite" });
+        } else {
+            res.status(404).json({ code: 404, msg: 'Unable to add favourites' });
+        }
+    }
+});
+
+router.get('/:id/favourites', async (req, res) => {
+    const user = await User.findById(req.params.id);
+    if (user) {
+        res.status(200).json(user.favourites);
+    } else {
+        res.status(404).json({ code: 404, msg: 'Unable to find favourites' });
     }
 });
 
