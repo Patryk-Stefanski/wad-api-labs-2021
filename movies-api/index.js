4a1e91758cf09160b2279549a6f52d7eab0879b9
Patryk-Stefanski <patrykstefanski00@gmail.com>
2021-11-25 14:48:49 +0000

    Topic 10 - Passport

--- movies-api/index.js
+++ movies-api/index.js
@@ -6,7 +6,7 @@ import express from 'express';
 import moviesRouter from './api/movies';
 import genresRouter from './api/genres';
 import session from 'express-session';
-import authenticate from './authenticate';
+import passport from './authenticate';
 
 dotenv.config();
 
@@ -24,15 +24,11 @@ const app = express();
 const port = process.env.PORT;
 
 //session middleware
-app.use(session({
-  secret: 'ilikecake',
-  resave: true,
-  saveUninitialized: true
-}));
+app.use(passport.initialize());
 
 app.use(express.json());
 
-app.use('/api/movies', authenticate ,moviesRouter);
+app.use('/api/movies', passport.authenticate('jwt', {session: false}), moviesRouter);
 
 app.use('/api/genres', genresRouter);
 
